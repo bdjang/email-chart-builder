b9ba87db50bca68307a44e950c7c731b0cceaba7
bdjang <bdjang@gmail.com>
2023-01-20 21:20:38 -0500

    Add keydown functionality, update dark mode logic

--- chart-functionality.js
+++ chart-functionality.js
@@ -35,8 +35,107 @@ const stacked4BarsNodes1 = document.querySelectorAll("tr.stacked4Bars table.char
 const stacked4BarsNodes5 = document.querySelectorAll("tr.stacked4Bars table.chartMobile td:nth-of-type(5)");
 const negativeBarsNodes1 = document.querySelectorAll("tr.negativeBars table.chartMobile td:nth-of-type(1)");
 const negativeBarsNodes2 = document.querySelectorAll("tr.negativeBars table.chartMobile td:nth-of-type(2)");
+const splitBarsNodes1 = document.querySelectorAll("tr.splitBars table.chartMobile td:nth-of-type(1)");
+const splitBarsNodes4 = document.querySelectorAll("tr.splitBars table.chartMobile td:nth-of-type(4)");
 const combo1BarsNodes1 = document.querySelectorAll("tr.combo1Bars table.chartMobile td:nth-of-type(1)");
 const combo1BarsNodes3 = document.querySelectorAll("tr.combo1Bars table.chartMobile td:nth-of-type(3)");
+const bodyEl = document.querySelectorAll("body");
+document.addEventListener("keydown", () => {
+    if (event.keyCode == 67 && document.activeElement == bodyEl[0]) {
+        chartSelect.focus();
+    }
+    if (event.keyCode == 81) {
+        chartSelect.blur();
+        // document.activeElement == bodyEl[0];
+    }
+    if (event.keyCode == 68 && document.activeElement == bodyEl[0]) {
+        chartSelect.blur();
+        document.body.classList.toggle("darkStyles");
+        document.body.classList.replace("lightStyles", "darkStyles");
+        prog1Row1a.classList.toggle("darkStyles");
+        prog1Row1a.classList.replace("lightStyles", "darkStyles");
+        prog1Row2a.classList.toggle("darkStyles");
+        prog1Row2a.classList.replace("lightStyles", "darkStyles");
+        prog1Row3a.classList.toggle("darkStyles");
+        prog1Row3a.classList.replace("lightStyles", "darkStyles");
+        prog1Row4a.classList.toggle("darkStyles");
+        prog1Row4a.classList.replace("lightStyles", "darkStyles");
+        prog1Row5a.classList.toggle("darkStyles");
+        prog1Row5a.classList.replace("lightStyles", "darkStyles");
+        prog2Row1a.classList.toggle("darkStyles");
+        prog2Row1a.classList.replace("lightStyles", "darkStyles");
+        prog2Row2a.classList.toggle("darkStyles");
+        prog2Row2a.classList.replace("lightStyles", "darkStyles");
+        prog2Row3a.classList.toggle("darkStyles");
+        prog2Row3a.classList.replace("lightStyles", "darkStyles");
+        prog2Row4a.classList.toggle("darkStyles");
+        prog2Row4a.classList.replace("lightStyles", "darkStyles");
+        prog2Row5a.classList.toggle("darkStyles");
+        prog2Row5a.classList.replace("lightStyles", "darkStyles");
+        horiz4Caption.classList.toggle("darkStyles");
+        horiz4Caption.classList.replace("lightStyles", "darkStyles");
+        for (var i = 0; i < scrollBox.length; i++) {
+            scrollBox[i].classList.toggle("darkStyles");
+            scrollBox[i].classList.replace("lightStyles", "darkStyles");
+            textarea[i].classList.toggle("darkStyles");
+            textarea[i].classList.replace("lightStyles", "darkStyles");
+            previewBox[i].classList.toggle("darkStyles");
+            previewBox[i].classList.replace("lightStyles", "darkStyles");
+        }
+        for (var x = 0; x < horiz1Bars.length; x++) {
+            horiz1BarsNodes1[x].classList.toggle("darkStyles");
+            horiz1BarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            horiz1BarsNodes2[x].classList.toggle("darkStyles");
+            horiz1BarsNodes2[x].classList.replace("lightStyles", "darkStyles");
+            horiz2BarsNodes3[x].classList.toggle("darkStyles");
+            horiz2BarsNodes3[x].classList.replace("lightStyles", "darkStyles");
+            horiz3BarsNodes1[x].classList.toggle("darkStyles");
+            horiz3BarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            horiz3BarsNodes3[x].classList.toggle("darkStyles");
+            horiz3BarsNodes3[x].classList.replace("lightStyles", "darkStyles");
+            horiz4BarsNodes1[x].classList.toggle("darkStyles");
+            horiz4BarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            horiz4BarsNodes2[x].classList.toggle("darkStyles");
+            horiz4BarsNodes2[x].classList.replace("lightStyles", "darkStyles");
+            stacked1BarsNodes1[x].classList.toggle("darkStyles");
+            stacked1BarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            stacked1BarsNodes4[x].classList.toggle("darkStyles");
+            stacked1BarsNodes4[x].classList.replace("lightStyles", "darkStyles");
+            stacked2BarsNodes1[x].classList.toggle("darkStyles");
+            stacked2BarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            stacked2BarsNodes4[x].classList.toggle("darkStyles");
+            stacked2BarsNodes4[x].classList.replace("lightStyles", "darkStyles");
+            stacked3BarsNodes1[x].classList.toggle("darkStyles");
+            stacked3BarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            stacked4BarsNodes1[x].classList.toggle("darkStyles");
+            stacked4BarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            stacked4BarsNodes5[x].classList.toggle("darkStyles");
+            stacked4BarsNodes5[x].classList.replace("lightStyles", "darkStyles");
+            stacked1BarsNodes3[x].classList.toggle("darkBorder");
+            negativeBarsNodes1[x].classList.toggle("darkStyles");
+            negativeBarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            negativeBarsNodes2[x].classList.toggle("darkStyles");
+            negativeBarsNodes2[x].classList.replace("lightStyles", "darkStyles");
+            splitBarsNodes1[x].classList.toggle("darkStyles");
+            splitBarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+            splitBarsNodes4[x].classList.toggle("darkStyles");
+            splitBarsNodes4[x].classList.replace("lightStyles", "darkStyles");
+        }
+        for (var y = 0; y < combo1BarsNodes1.length; y++) {
+            combo1BarsNodes1[y].classList.toggle("darkStyles");
+            combo1BarsNodes1[y].classList.replace("lightStyles", "darkStyles");
+            combo1BarsNodes3[y].classList.toggle("darkStyles");
+            combo1BarsNodes3[y].classList.replace("lightStyles", "darkStyles");
+        }
+        if (lightMode.style.display == "none") {
+            lightMode.style.display = "inline-block";
+            darkMode.style.display = "none";
+        } else if (lightMode.style.display == "inline-block") {
+            lightMode.style.display = "none";
+            darkMode.style.display = "inline-block";
+        }
+    }
+});
 darkMode.addEventListener("click", () => {
     document.body.classList.toggle("darkStyles");
     document.body.classList.replace("lightStyles", "darkStyles");
@@ -104,6 +203,10 @@ darkMode.addEventListener("click", () => {
         negativeBarsNodes1[x].classList.replace("lightStyles", "darkStyles");
         negativeBarsNodes2[x].classList.toggle("darkStyles");
         negativeBarsNodes2[x].classList.replace("lightStyles", "darkStyles");
+        splitBarsNodes1[x].classList.toggle("darkStyles");
+        splitBarsNodes1[x].classList.replace("lightStyles", "darkStyles");
+        splitBarsNodes4[x].classList.toggle("darkStyles");
+        splitBarsNodes4[x].classList.replace("lightStyles", "darkStyles");
     }
     for (var y = 0; y < combo1BarsNodes1.length; y++) {
         combo1BarsNodes1[y].classList.toggle("darkStyles");
@@ -150,6 +253,8 @@ lightMode.addEventListener("click", () => {
         stacked1BarsNodes3[x].classList.toggle("darkBorder");
         negativeBarsNodes1[x].classList.replace("darkStyles", "lightStyles");
         negativeBarsNodes2[x].classList.replace("darkStyles", "lightStyles");
+        splitBarsNodes1[x].classList.replace("darkStyles", "lightStyles");
+        splitBarsNodes4[x].classList.replace("darkStyles", "lightStyles");
     }
     for (var y = 0; y < combo1BarsNodes1.length; y++) {
         combo1BarsNodes1[y].classList.replace("darkStyles", "lightStyles");
