0c0fd9c864b8aebc3da77ace8d628730a7d188fc
bdjang <bdjang@gmail.com>
2023-03-31 21:26:33 -0400

    Update selectElement logic

--- scripts/chart-functionality.js
+++ scripts/chart-functionality.js
@@ -9,10 +9,10 @@ selectElement.addEventListener("change", () => {
             charts[i].style.display = "none";
             charts[selectElement.selectedIndex - 1].style.display = "block";
         }
-        if (selectElement.selectedIndex >= 19 && selectElement.selectedIndex < 24) {
+        if (selectElement.selectedIndex >= 20 && selectElement.selectedIndex < 25) {
             chartEmoji.style.transform = "rotate(0deg)"; // For vertical bar charts
             chartEmoji.style.marginRight = "13px";
-        } else if (selectElement.selectedIndex < 19) {
+        } else if (selectElement.selectedIndex < 20) {
             chartEmoji.style.transform = "rotate(90deg)"; // For horizontal bar charts
             chartEmoji.style.marginRight = "19px";
         }
