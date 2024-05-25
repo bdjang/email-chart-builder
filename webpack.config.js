5ca0b30517283291066b858fe345e75518887fe0
bdjang <bdjang@gmail.com>
2024-05-25 17:54:15 -0400

    Update webpack.config.js to use image assets

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
